860f2d43e0fe28b5b05b1216763100567be76425
Mukles <Mukleshossen1763@gmail.com>
2023-02-03 14:55:57 +0600

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+const Index = () => {
+  return (
+    <section>
+      <div className="container">
+        <ul>
+          <li>Portfolio</li>
+          <li>Portfolio</li>
+          <li>Portfolio</li>
+          <li>Portfolio</li>
+          <li>Portfolio</li>
+          <li>Portfolio</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Index;
---
+++ src/pages/project.tsx
@@ -0,0 +1,5 @@
+const Project = () => {
+  return <h1>Hellow from Project</h1>;
+};
+
+export default Project;
