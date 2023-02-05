a6c7847bdaa2f00f6992dc3f9fcb7e4d8fe7b240
Mukles <Mukleshossen1763@gmail.com>
2023-02-05 15:26:36 +0600

    warning remove

--- src/Components/social.tsx
+++ src/Components/social.tsx
@@ -2,16 +2,16 @@ const Social = () => {
   return (
     <ul className="social">
       <li>
-        <a href="">Instagram</a>
+        <a href="/">Instagram</a>
       </li>
       <li>
-        <a href="">Linkdin</a>
+        <a href="/">Linkdin</a>
       </li>
       <li>
-        <a href="">GitHub</a>
+        <a href="/">GitHub</a>
       </li>
       <li>
-        <a href="">Email</a>
+        <a href="/">Email</a>
       </li>
     </ul>
   );
